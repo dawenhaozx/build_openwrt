7eb1c0d83c72cc0e378d5d4b17621287c9cf714c
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-01-19 20:43:53 +0800

    u

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
+# Add luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
