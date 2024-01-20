e190f8396be73bb85ad7cf4b7cd3fa76fec18be4
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-01-20 23:48:36 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -12,5 +12,5 @@
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 #passwall2
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-#echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
